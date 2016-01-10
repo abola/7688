61688ea9e8816d91825b72f3410970aadd05b3a4
Abola Lee <abola921@gmail.com>
2016-01-11 00:07:51 +0800

    Update 8x8_led_matrix_show_number.js

--- example/8x8_led_matrix_show_number.js
+++ example/8x8_led_matrix_show_number.js
@@ -1,1 +1,65 @@
+/**
+ * Linkit 7688 範例
+ * 使用 GPIO 控制 8x8 LED Matrix (共陽) 
+ * 顯示數字
+ * 
+ * @author Abola Lee 
+ * @version 1.0
+ * @since 2016-01-10
+ * 
+ * @link http://www.gibar.co/2016/01/7688-8x8-led-matrix.html
+ */
+var mraa = require('mraa');
 
+// 腳位命名參考
+//  
+//    
+//    \Col 1   2   3   4   5   6   7   8
+//Row  \ _13__03__04__10__06__11__15__16_  (針腳)
+// 1  09|                               |
+// 2  14|                               |
+// 3  08|                               |
+// 4  12|              8x8              |
+// 5  01|           LED Matrix          |
+// 6  07|                               |
+// 7  02|                               |
+// 8  05|_______________________________|
+//   (針腳)
+
+
+// 設定GPIO 接腳
+var col = [
+      new mraa.Gpio(2)
+      , new mraa.Gpio(16)
+      , new mraa.Gpio(17)
+      , new mraa.Gpio(44)
+      , new mraa.Gpio(5)
+      , new mraa.Gpio(46)
+      , new mraa.Gpio(0)
+      , new mraa.Gpio(1) 
+    ],
+    row = [
+      new mraa.Gpio(37)
+      , new mraa.Gpio(3)
+      , new mraa.Gpio(13)
+      , new mraa.Gpio(45)
+      , new mraa.Gpio(14)
+      , new mraa.Gpio(12)
+      , new mraa.Gpio(15)
+      , new mraa.Gpio(4)      
+    ];
+    
+// 預設全亮測試
+for(var idx=0; idx<8; idx++) col[idx].dir(mraa.DIR_OUT_HIGH);
+for(var idx=0; idx<8; idx++) row[idx].dir(mraa.DIR_OUT_LOW);
+
+var resetFlag=false;
+/**
+ * 回復全暗
+ */
+function reset(){
+  resetFlag = true; // 
+  for(var idx=0; idx<8; idx++) col[idx].write(0);
+  for(var idx=0; idx<8; idx++) row[idx].write(0);
+  resetFlag=false;
+}
