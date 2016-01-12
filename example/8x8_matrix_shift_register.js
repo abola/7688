d8bc8e43d070691742e9642847c7001b23696236
Abola Lee <abola921@gmail.com>
2016-01-12 20:25:43 +0800

    Update 8x8_matrix_shift_register.js

--- example/8x8_matrix_shift_register.js
+++ example/8x8_matrix_shift_register.js
@@ -31,3 +31,55 @@ var DS   = new m.Gpio(15),
 DS  .dir(m.DIR_OUT);
 STCP.dir(m.DIR_OUT);
 SHCP.dir(m.DIR_OUT);
+
+function setOutput(value) {
+  valeurMirroir=value;
+  for( var i = 0 ; i < 8 ; i++) {
+    
+    if( 0x80 == (value & 0x80) ){
+      DS.write(1);
+    }
+    else {
+      DS.write(0);
+    }
+    // clock data
+    SHCP.write(0);
+    SHCP.write(1);
+    // 
+    value = value << 1;
+  }
+  // latch data
+  STCP.write(1);
+  STCP.write(0);
+}
+
+
+/**
+ * 逐顆亮燈
+ */
+function setBit(bitIndex) {
+  setOutput(valeurMirroir | (1 << bitIndex));
+}
+/**
+ * 逐顆熄燈
+ */
+function clrBit(bitIndex) {
+  setOutput(valeurMirroir & ~(1 << bitIndex));
+}
+
+
+// start it !
+setInterval(
+  function() {
+    if(chaserIndex > 7){
+      clrBit(chaserIndex - 8);
+    }
+    else {
+      setBit(chaserIndex);
+    }
+    
+    chaserIndex++;
+    if(chaserIndex > 15) 
+      chaserIndex=0;
+  }
+  , sequence );
